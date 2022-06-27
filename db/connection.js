84eb6e9a845ac29adc8f9b61e3c50a9ac3056156
Michael Ramos <michaelaramos182@gmail.com>
2022-06-27 15:33:20 -0600

    Added in port and db connection to mysql in connection.js

---
+++ db/connection.js
@@ -0,0 +1,18 @@
+const mySQL = require('mysql2');
+require('dotenv').config();
+
+const connection = mySQL.createConnection(
+    {
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        database: "track-my-team-db",
+    },
+);
+
+ connection.connect((error) => {
+     console.log(error);
+     if(error) throw error;
+ });
+
+ module.exports = connection
